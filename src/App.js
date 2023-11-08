9c17c9bea5a794cadb453bc0a404fa7804140470
rompestomp <playzlama1@gmail.com>
2023-11-08 11:03:25 +0100

    Update App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
+    <section className='firstsection'> 
+    <header className="App-header">
+            <h1>
+              MinVeiLeder
+            </h1>
+          </header>
+       </section> 
+
+
+    <section className='firstsection'> 
+<header className="App-header">
+        <h1>
+          MinVeiLeder
+        </h1>
       </header>
-    </div>
+   </section> 
+
   );
 }
 
