1ac0e00bdf364d5d21ffd6ef39288158d31e60ef
rompestomp <playzlama1@gmail.com>
2023-11-09 08:53:59 +0100

    allt nytt

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import logo from './logo.svg';
-import './App.css'; 
+import React from 'react';
+import logo from './Pictures/logo.png'; // Your logo file
+import logo2 from './Pictures/logo_stor.png'; // Your logo file
+import login from './Enter.svg';
+import search from './MagnifyingGlass.svg';
+import menu from './MenuHamburger.svg';
+
+
+
+import './App.css';
 
 function App() {
   return (
-    
-    <section className='firstsection'> 
-    <header className="App-header">
-            <h1>
-              MinVeiLeder
-            </h1>
-          </header>
-       </section> 
+    <><div className="App">
+      <header className="App-header">
+        <img src={logo} alt="MinVei Logo" className="App-logo" />
 
+        <nav className="App-navigation">
+          <a href="#minveileder" className="nav-link">MinVeileder</a>
+          <a href="#livssituasjon" className="nav-link">Livssituasjon</a>
+          <a href="#samarbeidspartner" className="nav-link">Samarbeidspartner</a>
+        </nav>
 
-    <section className='firstsection'> 
-<header className="App-header">
-        <h1>
-          MinVeiLeder
-        </h1>
+        <div className="App-header-actions">
+          <button className="button menu-button"> <img src={menu} className="icon" />Meny</button>
+          <button className="button search-button"> <img src={search} className="icon" />Søk</button>
+          <button className="button login-button"> <img src={login} className="icon" /> Logg inn</button>
+        </div>
       </header>
-   </section> 
+
+
+
+
+      {/* Section for 'MinVeiLeder' */}
+      <section className="firstsection">
+        <center>
+          <img src={logo2} alt="MinVei Logo" className="logo" width="25%" height="10%" />
+        </center>
+      </section>
+    </div>
+        <center>
+<p>Lurer du på hva du kan ha rett på av hjelp og økonomisk støtte fra det offentlige i Norge?
+  <br/>
+  Gjennom min veileder kan du utforske dette. </p>
+
+        </center>
+     </>
 
   );
 }
+
 
 export default App;
