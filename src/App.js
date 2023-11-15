703c01a823d49037c940e17477160d972dfa11d2
oscarostby <118980639+oscarostby@users.noreply.github.com>
2023-11-15 14:39:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import login from './Enter.svg';
 import search from './MagnifyingGlass.svg';
 import menu from './MenuHamburger.svg';
 
-
-
+import pilned from "./pilned.svg";
+import "./dropdown.js";
 import './App.css';
+import AccordionItem from './AccordionItems.js';
+
+
 
 function App() {
   return (
-    <><div className="App">
+    <><><><div className="App">
       <header className="App-header">
         <img src={logo} alt="MinVei Logo" className="App-logo" />
 
@@ -38,13 +41,51 @@ function App() {
         </center>
       </section>
     </div>
-        <center className='p-tekst'>
-        <p>Lurer du på hva du kan ha rett på av hjelp og økonomisk støtte fra det offentlige i Norge?
-        <br/>
-        Gjennom min veileder kan du utforske dette. </p>
+      <center className='p-tekst'>
+        <p>Lurer du på hva du kan ha rett på av hjelp og økonomisk
+           <br />
+           støtte fra det offentlige i Norge?
+          <br />
+          Gjennom min veileder kan du utforske dette. </p>
+      </center>
 
-        </center>
-     </>
+
+      {/* dropdown buttons*/}
+
+
+
+       <div class="accordion">
+        <div class="accordion-item">
+          <p class="accordion-header">Hva kan min veileder hjelpe med?</p>
+          <div class="panesl">
+            <p>Detaljert informasjon om hva veilederen kan hjelpe med.</p>
+          </div>
+        </div>
+
+        <div class="accordion-item">
+          <p class="accordion-header"><img src={pilned} /> er min veileder?</p>
+          <div class="panel">
+            <p>Informasjon om veilederens rolle og funksjoner.</p>
+          </div>
+        </div>
+      </div> 
+
+
+
+
+      {/*code for bottom text and buttons under the dropdown buttons*/}
+      
+    </><div className='bottom-text'>
+        <p className='b-text'>Det er viktig å understreke at MinVeileder kun kan foreslå
+          <br />
+          ytelser/tjenester du kan ha krav på. For å få endelig svar
+          <br />
+          må du sende inn en søknad.</p>
+      </div>
+
+      </><div>
+        <button className='start-veileder'><b>Start veilederen</b></button>   <button className='help-b'><b>Få hjelp med veileder</b></button>
+        </div></>
 
   );
 }
