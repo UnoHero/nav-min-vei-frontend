68ccbef47a9dce44ad9ecd0cdb57722419de5616
Maamua06 <115535704+Maamua06@users.noreply.github.com>
2023-12-06 22:39:18 +0100

    ikke godkjenn

--- src/footer.js
+++ src/footer.js
@@ -2,13 +2,6 @@ import React from "react";
 import ScrollToTop from "./scrollToTop";
 import "./App.css";
 
-const ImageDisplayed = () => (
-  <img
-    src="your_image_source_here"
-    alt="Description of your image"
-    style={{ width: "100%", height: "auto" }}
-  />
-);
 
 class Footer extends React.Component {
   render() {
@@ -45,19 +38,6 @@ class Footer extends React.Component {
                 Nyheter, pressemeldinger og pressekontakt
               </a>
             </p>
-            <p>
-              <a href="https://www.nav.no/no/nav-og-samfunn/statistikk-analyse-og-fou">
-                Statistikk, analyse og FoU
-              </a>
-            </p>
-            <p>
-              <a href="https://lovdata.no/nav/">Lover og regler</a>
-            </p>
-            <p>
-              <a href="https://www.nav.no/no/nav-og-samfunn/om-nav/om-nav">
-                Om NAV
-              </a>
-            </p>
           </div>
           <div className="footer-column">
             <h1>Andre språk</h1>
@@ -81,16 +61,7 @@ class Footer extends React.Component {
               <a href="https://www.nav.no/#">Del skjerm med veileder</a>
             </p>
 
-            <h1 className='h1-column-1'>Kontaktinformasjon</h1>
-            <p className='p-footer-column-1'><a href="https://www.nav.no/kontaktoss">Kontakt Oss</a></p>
-            <br/>
-            <p className='p-footer-column-1'><a href="https://www.nav.no/no/nav-og-samfunn/kontakt-nav/nav-i-ditt-fylke">NAV i ditt fylke</a></p>
-            <br/>
-            <p className='p-footer-column-1'><a href="https://www.nav.no/no/nav-og-samfunn/kontakt-nav/kurs-fra-nav">Kurs fra NAV</a></p>
-            <br/>
-            <p className='p-footer-column-1'><a href="https://www.nav.no/person/kontakt-oss/nb/tilbakemeldinger/">Klage, tilbakemelding, ros</a></p>
-          </div>
-          <div className="column-2">
+            
             <h1 className='h1-column-2-3'>Nyheter og presse</h1>
             <p className='p-footer'><a href="https://www.nav.no/samarbeidspartner/presse">Nyheter, pressemeldinger og pressekontakt</a></p>
             <br/>
@@ -100,19 +71,7 @@ class Footer extends React.Component {
             <p className='p-footer'><a href="https://lovdata.no/nav/">Lover og regler</a></p>
             <p className='p-footer'><a href="https://www.nav.no/no/nav-og-samfunn/om-nav/om-nav">Om NAV</a></p>
           </div>
-          <div className="column-3">
-            <h1 className='h1-column-2-3'>Andre språk</h1>
-            <p className='p-footer'><a href="https://www.nav.no/en/home">English</a></p>
-            <p className='p-footer'><a href="https://www.nav.no/person/bestilling-av-samisk-samtale">Samegialla</a></p>
-            <br/>
-            <br/>
-            <p className='p-footer'><a href="https://www.nav.no/no/nav-og-samfunn/om-nav/personvern-i-arbeids-og-velferdsetaten">Personvern og informasjonskapsler</a></p>
-            <p className='p-footer'><a href="https://www.nav.no/tilgjengelighet">Tilgjenglighet</a></p>
-            <p className='p-footer'><a href="https://www.nav.no/#">Del skjerm med veileder</a></p>
-
-          </div>
         </div>
-        <ImageDisplayed />
       </footer>
     );
   }
