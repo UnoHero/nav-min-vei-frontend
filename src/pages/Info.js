6ec18e4a3aea6c0a8dea02eecf50aab9a9022abd
mleitejens <mleitejensen@gmail.com>
2024-01-31 18:59:57 +0100

    Made styling changes to the info page

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -4,9 +4,9 @@ import { useSpring } from 'react-spring';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import "@navikt/ds-css";
-import { Stepper } from "@navikt/ds-react";
 import Section from '../components/section';
 import Main from '../components/Info_main'; // Ensure this is imported only once
+import { InformationIcon, CheckmarkIcon } from '@navikt/aksel-icons';
  
 const Body = styled.div`
   background-color: rgb(211, 230, 237);
@@ -17,28 +17,14 @@ const MainContent = styled.div`
   display: flex;
   justify-content: center;
 `;
- 
-const Layout = styled.div`
-  display: flex;
-  align-items: flex-start;
-`;
- 
-const StepperContainer = styled.div`
-  margin-right: 20px; // Space between stepper and boxes
-`;
- 
-const BoxContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px; // Space between boxes
-`;
 
 const Circle = styled.div`
+  position: relative;
   border: 2px solid white;
   background-color: white;
   border-radius: 50%;
   color: #0067C5;
-  
+  z-index: 1;
   margin: 0px 3px 0px 3px;
   text-align: center;
   display: block;
@@ -49,7 +35,16 @@ const Circle = styled.div`
 const TextBox = styled.div`
   background-color: white;
   border-radius: 5px;
-  padding: 5px;
+  padding: 1rem 8rem;
+`
+
+const Line = styled.div`
+  width: 0.3rem;
+  height: 95%;
+  background-color: #CCE1FF;
+  position: relative;
+  left: 15px;
+  z-index: 0;
 `
   
 const List = styled.ul`
@@ -58,10 +53,18 @@ const List = styled.ul`
   
 const Item = styled.li`
   display: grid;
-  grid-template-columns: 50px 400px;
+  grid-template-columns: 50px 600px;
   margin: 20px 0px 20px 0px;
 `;
- 
+
+const StepTitle = styled.div`
+  font-size: 1.75rem;
+`;
+
+const StepOfSteps = styled.div`
+  font-size: 1rem;
+`;
+
 // Component for each box next to Stepper steps
 const StepBox = ({ isActive }) => {
   const animationProps = useSpring({
@@ -76,7 +79,6 @@ const StepBox = ({ isActive }) => {
  
 const Info = () => {
   const [activeStep, setActiveStep] = useState(null);
-  const steps = 5;
  
   return (
     <>
@@ -87,22 +89,44 @@ const Info = () => {
 
       <List>
         <Item>
-          <Circle>I</Circle>
-          <TextBox>Info tekst box werwerwer wefwefew  werwerwer wefwefew werwerwer wefwefew werwerwer wefwefew werwerwer wefwefewrferfrefwerwerwer werwerwer wefwefew werwerwer wefwefew werwerwer wefwefew werwerwer wefwefew </TextBox>
+          <div><Circle><InformationIcon title="a11y-title" fontSize="1.5rem" /></Circle><Line></Line></div>
+          <TextBox>
+            <StepOfSteps>Samling av din informasjon via</StepOfSteps>
+            <StepTitle>Datahenting</StepTitle>
+            
+          </TextBox>
         </Item>
+
         <Item>
-          <Circle>1</Circle>
-          <TextBox>Steg 1 tekst box</TextBox>
+        <div><Circle>1</Circle><Line></Line></div>
+          <TextBox>
+            <StepOfSteps>Steg 1 av 3</StepOfSteps>
+            <StepTitle>Om meg</StepTitle>
+          </TextBox>
         </Item>
+
         <Item>
-          <Circle>2</Circle>
-          <TextBox>Steg 2 tekst box</TextBox></Item>
+          <div><Circle>2</Circle><Line></Line></div>
+          <TextBox>
+            <StepOfSteps>Steg 2 av 3</StepOfSteps>
+            <StepTitle>Velg livssituasjon</StepTitle>
+          </TextBox>
+        </Item>
+
         <Item>
-          <Circle>3</Circle>
-          <TextBox>Steg 3 tekst box</TextBox></Item>
+          <div><Circle>3</Circle><Line></Line></div>
+          <TextBox>
+            <StepOfSteps>Steg 3 av 3</StepOfSteps>
+            <StepTitle>Spørsmål til min livssituasjon</StepTitle>
+          </TextBox>
+        </Item>
+        
         <Item>
-          <Circle>!</Circle>
-          <TextBox>Ferdig tekst box</TextBox></Item>
+          <Circle><CheckmarkIcon title="a11y-title" fontSize="1.5rem" /></Circle>
+          <TextBox>
+            <StepTitle>Mine resultater</StepTitle>
+          </TextBox>
+        </Item>
       </List>
 
       </MainContent>
