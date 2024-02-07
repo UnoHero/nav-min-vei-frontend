c48889f4d762e0884b0fb2b6c741d425604f8fde
mleitejens <mleitejensen@gmail.com>
2024-02-07 10:20:48 +0100

    Added activeStep and go to next step button

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -37,6 +37,7 @@ const TextBox = styled.div`
   background-color: white;
   border-radius: 5px;
   padding: 1rem 8rem;
+  cursor: ${props => props.cursor};
 `
 
 const Line = styled.div`
@@ -69,7 +70,8 @@ const StepHeader = styled.div`
 const StepText = styled.div`
  font-size: 1rem;
  margin: 2rem 0rem;
-`
+ `
+ //color: ${props => props.isError ? "red" : "green"};
 
 const RadioBox = styled.div`
   padding: 3rem 2rem;
@@ -79,17 +81,13 @@ const RadioBox = styled.div`
 
 `;
 
-// Component for each box next to Stepper steps
-const StepBox = ({ isActive }) => {
-  const animationProps = useSpring({
-    height: isActive ? 'auto' : 0, // Expand height if active
-    opacity: isActive ? 1 : 0,
-    from: { height: 0, opacity: 0 },
-  });
- 
-  // Conditionally render the Main component if isActive is true
-  return isActive ? <Main style={animationProps} /> : null;
-};
+const NextStepButton = styled.button`
+  border-radius: 4px;
+  border: 0px;
+  padding: 12px 20px;
+  background-color: #0067C5;
+  color: white;
+`
  
 const Info = () => {
   const [activeStep, setActiveStep] = useState(null);
@@ -104,67 +102,72 @@ const Info = () => {
       <List>
         <Item>
           <div><Circle><InformationIcon title="a11y-title" fontSize="1.5rem" /></Circle><Line></Line></div>
-          <TextBox>
+          <TextBox cursor={activeStep === "info" ?  "default" : "pointer"} onClick={() => setActiveStep("info")}>
             <StepHeader>Samling av din informasjon via</StepHeader>
             <StepTitle>Datahenting</StepTitle>
 
-            <StepHeader>Hente data det offentlige har om deg</StepHeader>
-            <StepText>
-              Dersom du ønsker har du mulighet til å hente data fra 
-              ulike offentlige etater slik at utfyllingen av veilederen 
-              blir enklere for deg.
-            </StepText>
-            
-            <Accordion>
-              <Accordion.Item>
-                <Accordion.Header>Hvorfor hente data</Accordion.Header>
-                <Accordion.Content>
-                Å hente data gjør at veilederen kan autofylle svar for deg, 
-                noe som betyr at du slipper å logge deg inn forskjellige steder 
-                for å finne informasjon du må fylle ut i veilederen. 
-                  <br/><br/>
-                Du kan også fint fylle ut informasjon selv, dersom du ikke ønsker å hente informasjon automatisk. 
-                </Accordion.Content>
-              </Accordion.Item>
-              <Accordion.Item>
-                <Accordion.Header>
-                  Hvordan virker datasamlingen?
-                </Accordion.Header>
-                <Accordion.Content>
-                  Datahenting fungerer ved at MinVeileder henter data som ulike 
-                  offentlige organisasjoner har på deg her i veilederen.
-                  <br/><br/>
-                  Informajsonen vil ikke bli delt på tvers av disse organisasjonene, og det er kun du som ser de samlet her.
-                </Accordion.Content>
-              </Accordion.Item>
-              <Accordion.Item>
-                <Accordion.Header>Hvem hentes infoen fra?</Accordion.Header>
-                <Accordion.Content>
-                MinVei henter informasjon fra følgende: 
-                <ul>
-                  <li>Folkeregisteret</li>
-                  <li>Skatteetaten</li>
-                  <li>NAV</li>
-                  <li>Lånekassen</li>
-                  <li>Brønnøysundregisteret</li>
-                  <li>Din tilhørende kommune</li>
-                  <li>Vegvesenet</li>
-                  <li>Helsenorge</li>
-                  <li>Samordna opptak</li>
-                  <li>Politiet</li>
-                  <li>Altinn</li>
-                  <li>Pasientsky</li>
-                </ul>
-                </Accordion.Content>
-              </Accordion.Item>
-            </Accordion>
-            
-            <RadioBox>
-              <RadioGroup legend="Ønsker du å hente data fra det offentlige?">
-                <Radio value="Yes">Ja</Radio>
-                <Radio value="No">Nei</Radio>
-              </RadioGroup>
-            </RadioBox>
+            {activeStep === "info" && 
+            <>
+              <StepHeader>Hente data det offentlige har om deg</StepHeader>
+              <StepText>
+                Dersom du ønsker har du mulighet til å hente data fra 
+                ulike offentlige etater slik at utfyllingen av veilederen 
+                blir enklere for deg.
+              </StepText>
+              
+              <Accordion>
+                <Accordion.Item>
+                  <Accordion.Header>Hvorfor hente data</Accordion.Header>
+                  <Accordion.Content>
+                  Å hente data gjør at veilederen kan autofylle svar for deg, 
+                  noe som betyr at du slipper å logge deg inn forskjellige steder 
+                  for å finne informasjon du må fylle ut i veilederen. 
+                    <br/><br/>
+                  Du kan også fint fylle ut informasjon selv, dersom du ikke ønsker å hente informasjon automatisk. 
+                  </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item>
+                  <Accordion.Header>
+                    Hvordan virker datasamlingen?
+                  </Accordion.Header>
+                  <Accordion.Content>
+                    Datahenting fungerer ved at MinVeileder henter data som ulike 
+                    offentlige organisasjoner har på deg her i veilederen.
+                    <br/><br/>
+                    Informajsonen vil ikke bli delt på tvers av disse organisasjonene, og det er kun du som ser de samlet her.
+                  </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item>
+                  <Accordion.Header>Hvem hentes infoen fra?</Accordion.Header>
+                  <Accordion.Content>
+                  MinVei henter informasjon fra følgende: 
+                  <ul>
+                    <li>Folkeregisteret</li>
+                    <li>Skatteetaten</li>
+                    <li>NAV</li>
+                    <li>Lånekassen</li>
+                    <li>Brønnøysundregisteret</li>
+                    <li>Din tilhørende kommune</li>
+                    <li>Vegvesenet</li>
+                    <li>Helsenorge</li>
+                    <li>Samordna opptak</li>
+                    <li>Politiet</li>
+                    <li>Altinn</li>
+                    <li>Pasientsky</li>
+                  </ul>
+                  </Accordion.Content>
+                </Accordion.Item>
+              </Accordion>
+              
+              <RadioBox>
+                <RadioGroup legend="Ønsker du å hente data det offentlige har om deg for å autofylle svar i veilederen?">
+                  <Radio value="Yes">Ja, jeg ønsker å hente data fra offentlige tjenester</Radio>
+                  <Radio value="No">Nei, jeg ønsker å fylle ut selv</Radio>
+                </RadioGroup>
+              </RadioBox>
+              <NextStepButton>Gå Videre</NextStepButton>
+              </>
+            }
             
           </TextBox>
         </Item>
