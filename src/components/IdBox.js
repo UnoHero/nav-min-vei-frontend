ba421bf5130573e651443553e10ee8900715dbf6
mleitejens <mleitejensen@gmail.com>
2024-03-06 14:31:34 +0100

    Added animations to stepOne

--- src/components/IdBox.js
+++ src/components/IdBox.js
@@ -1,20 +1,51 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef, useLayoutEffect } from "react";
 import { Radio, RadioGroup } from "@navikt/ds-react";
 import {InputLabel, InputBox } from "../components/styledComponents"
+import { useSpring, animated } from '@react-spring/web'
 
-const IdBox = ({setNumber}) => {
+const IdBox = ({setNumber, setSmallBox, setSmallBoxSize}) => {
 
     const [selectedValue, setSelectedValue] = useState('');
     // "id" becomes the social security number that is writen in the text filed
 
     const handleRadioChange = (e) => {
         setSelectedValue(e.target.value);
-        console.log(e.target.value);
     }
+
+    const [boxed, setBoxed] = useState(true)
+    const boxRef = useRef(null)
+
+    const [springs, api] = useSpring(() => ({
+        from: { height: 0, opacity: 0 },
+        
+    }))
+
+    useEffect(() => {
+        setSmallBoxSize(boxed ? boxRef.current?.offsetHeight : 0)
+        api.start({
+            to: {height: boxed ? boxRef.current?.offsetHeight : 0, opacity: boxed ? 1 : 0,},
+            config: {
+                mass: 1,
+                tension: 170, 
+                friction: 26
+            },
+        })
+    },[api, boxed,])
+
+    useEffect(() => {
+        if(selectedValue === "Yes"){
+            setBoxed(true)
+            setSmallBox(true)
+        }
+        if(selectedValue !== "Yes"){
+            setBoxed(false)
+            setSmallBox(false)
+        }
+    }, [selectedValue])
 
 
     return(
-        <div onClick={(e) =>  e.stopPropagation() }>
+        <div>
             <RadioGroup onClick={(e) => {
                 handleRadioChange(e)
             }} legend="Ønsker du å hente data det offentlige har om deg for å autofylle svar i veilederen?">
@@ -22,15 +53,15 @@ const IdBox = ({setNumber}) => {
                 <Radio value="No">Nei, jeg ønsker å fylle ut selv</Radio>
             </RadioGroup>
 
-            {selectedValue === 'Yes' && (
-            <div>
-            {/* Show something when 'Yes' is selected */}
-            <InputLabel>Fødselsnummer:</InputLabel>
-            <InputBox onChange={(e) => {
-                setNumber(e.target.value)
-                }}></InputBox>
-            </div>
-            )}
+            <animated.div style={{...springs}}>
+                <div ref={boxRef}>
+                {/* Show something when 'Yes' is selected */}
+                <InputLabel>Fødselsnummer:</InputLabel>
+                <InputBox onChange={(e) => {
+                    setNumber(e.target.value)
+                    }}></InputBox>
+                </div>
+            </animated.div>
         </div>
     )
 };
