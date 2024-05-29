0323bff381e065bc0a18d92d9ce2c7d425c2153e
Martin <martin@secker.no>
2024-05-29 11:18:06 +0200

    i just fixed a super mega new fancy mega design to the home page, now with special white box and everything inside! AMAZING SUPER NICE

--- src/components/section.js
+++ src/components/section.js
@@ -4,11 +4,9 @@ import logo2 from '../Pictures/logo_stor.png';
 class Section extends React.Component {
     render() {
         return ( 
-            <section className="firstsection">
                  <center>
-                    <img src={logo2} alt="MinVei Logo" className="logo" width="25%" height="10%" />
+                    <img src={logo2} alt="MinVei Logo" className="logo" width="45%" height="80%" />
                  </center>
-            </section>
          );
 
     }
